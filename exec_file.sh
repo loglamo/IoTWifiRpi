62b34c9e373f6681f7158b92aca8876eb254f617
nguyễn lan anh <loglamo@gmail.com>
2018-12-13 09:41:03 +0700

    exec, make default configuration with SSID and PW

---
+++ exec_file.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt-get install dnsmasq hostapd
+sudo systemctl stop dnsmasq
+sudo systemctl stop hostapd
+sudo cp ./dhcpcd.conf /etc/dhcpcd.conf
+sudo service dhcpcd restart
+sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+sudo cp ./dnsmasq.conf /etc/dnsmasq.conf
+sudo cp ./hostapd.conf /etc/hostapd/hostapd.conf
+sudo cp ./hostapd /etc/default/hostapd
+sudo systemctl start hostapd
+sudo systemctl start dnsmasq
+sudo cp ./sysctl.conf /etc/sysctl.conf
+sudo reboot
